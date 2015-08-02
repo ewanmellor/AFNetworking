93a22a7d534bd4e6f62d428923153110fff8f21e
Ewan Mellor <ewan@tipbit.com>
2015-08-02 00:58:39 -0700

    Add a .xcodeproj for a static iOS library.

---
+++ AFNetworking-iOS/AFNetworking-iOS/AFNetworking_iOS.h
@@ -0,0 +1,13 @@
+//
+//  AFNetworking_iOS.h
+//  AFNetworking-iOS
+//
+//  Created by Ewan Mellor on 8/1/15.
+//  Copyright (c) 2015 Ewan Mellor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFNetworking_iOS : NSObject
+
+@end
---
+++ AFNetworking-iOS/AFNetworking-iOS/AFNetworking_iOS.m
@@ -0,0 +1,13 @@
+//
+//  AFNetworking_iOS.m
+//  AFNetworking-iOS
+//
+//  Created by Ewan Mellor on 8/1/15.
+//  Copyright (c) 2015 Ewan Mellor. All rights reserved.
+//
+
+#import "AFNetworking_iOS.h"
+
+@implementation AFNetworking_iOS
+
+@end
